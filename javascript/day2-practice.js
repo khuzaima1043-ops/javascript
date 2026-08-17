bc06ad496c88db6eef6290ceb672015741e66f8b
khuzaima khan <khuzaima1043@gmail.com>
2026-08-17 16:33:27 +0500

    class 1 and 2 practice.js Done

---
+++ javascript/day2-practice.js
@@ -0,0 +1,12 @@
+let city = "dera Ismail Khan"
+console.log(city)
+let age = 18
+console.log(age)
+age = 19
+console.log(age)
+const country = "Pakistan"
+console.log(country)
+// country= "USA"
+// console.log(country)
+var myFavouriteGame = "Football"
+console.log(myFavouriteGame)
