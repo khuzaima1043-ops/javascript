8c350fcdc141fd3f66a7773cedc143ac1585baf2
khuzaima khan <khuzaima1043@gmail.com>
2026-08-17 16:09:50 +0500

    add day2.js with initial console.log statements

---
+++ javascript/day2.js
@@ -0,0 +1,10 @@
+console.log("javascript class 2")
+let a = "khuzii"
+console.log(a)
+a = "67"
+console.log(a)
+a = "khuzaima khan bamozaii"
+console.log(a)
+var b = "BAMOZAII"
+console.log(b)
+console.log ("khuzaima khan bamizaii is the bestdeveloper in the world")
