717d70506a305ec923e554a821cbfb29fc7b6cb1
khuzaima khan <khuzaima1043@gmail.com>
2026-08-18 14:31:49 +0500

    class 3-Practice let/const/var scope and redeclaration rules done

---
+++ javascript/class 3-practice.js
@@ -0,0 +1,27 @@
+let name = "khuzaimaKhan"
+console.log(name);
+let age = 18
+console.log(age);
+age = 19
+console.log(age);
+const city = "dera Ismail Khan"
+console.log(city);
+
+//    city = "islamabad"
+//    console.log(city);
+{
+    let myGame = "Football"
+    console.log(myGame);
+}
+    // console.log(myGame);
+{
+    var mobile = "Vivo1808"
+    console.log(mobile);
+}
+    console.log(mobile);
+
+    // let name = "Khan"
+    // console.log(name);
+
+    var mobile = "oppoA37"
+    console.log(mobile);
