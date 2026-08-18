06219433b86070685ec062e98cce185617adf1b5
khuzaima khan <khuzaima1043@gmail.com>
2026-08-18 17:29:57 +0500

    class 4 file add

---
+++ javascript/class04-datatypes-objects/day4-datatypes.js
@@ -0,0 +1,36 @@
+let name ="khuzaima"
+console.log(name);
+let age =  18
+console.log(age);
+
+let student = true
+console.log(student);
+ let x;
+ console.log(x);
+ let n = null
+ console.log(n);
+
+ console.log(typeof name )
+ console.log(typeof student);
+ console.log(typeof age);
+console.log(typeof n);
+ console.log(typeof x);
+ 
+            // Object
+
+let student = {
+    name : "khuzaima khan", age : 18, city : "dera ismail khan", language : "Urdu" ,
+}
+console.log(student);
+
+//  console.log(student.age);
+//  console.log(student.name);
+//  console.log(student.city);
+console.log(student["age"]);
+console.log(student["language"]);
+
+
+
+ 
+
+ 
