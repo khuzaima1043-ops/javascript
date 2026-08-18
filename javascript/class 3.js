74ca13418766ca7e6193a1fdd6037577117e1cd8
khuzaima khan <khuzaima1043@gmail.com>
2026-08-18 14:11:44 +0500

    Test let and var redeclaration behavior

--- javascript/class 3.js
+++ javascript/class 3.js
@@ -3,3 +3,17 @@
     console.log(food)
 }
     console.log(food)
+    {
+    var city = "dera Ismail khan"
+    console.log(city)
+}
+    console.log(city)
+
+    let subject = "Math"
+     let subject = "Science"
+     console.log(subject)
+
+    var subject = "Math"
+    var subject = "Science"
+     console.log(subject)
+     
