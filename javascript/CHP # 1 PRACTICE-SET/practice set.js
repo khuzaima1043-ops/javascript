f3c732d9d0e18e79965fe5e4e18e74b047781e5c
khuzaima khan <khuzaima1043@gmail.com>
2026-08-19 15:03:44 +0500

    Chapter 1 Practice set Done

---
+++ javascript/CHP # 1 PRACTICE-SET/practice set.js
@@ -0,0 +1,27 @@
+let a= 'khuzii'
+let b = 18
+console.log(a+b);
+
+console.log(typeof (a+b));
+
+const personkhan = {
+    name : "khuzaima" ,
+    city : "dera ismail "
+}
+    console.log(personkhan);
+
+    // personkhan= "hello"
+    // console.log(personkhan);
+
+        personkhan.age = 18
+        console.log(personkhan);
+        
+        
+   let dict = {
+    bank : "a bank of earth",
+    lake : "any similar body or pool of other liquid, as oil.",
+    mobile : " capable of moving or being moved readily.",
+}
+console.log(dict);
+
+
