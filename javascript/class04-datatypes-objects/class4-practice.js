2b0d89f11f5ec3d5dde2e01d33db579ce9e6df75
khuzaima khan <khuzaima1043@gmail.com>
2026-08-19 13:50:12 +0500

    class 4 practice done

---
+++ javascript/class04-datatypes-objects/class4-practice.js
@@ -0,0 +1,65 @@
+let name = "khuzaima Khan"
+console.log(name);
+let age = 18
+console.log(age);
+const city = "Pakistan"
+console.log(city);
+// city = "USA"
+// console.log(city);
+{
+    let game = "Football"
+    console.log(game);
+}
+// console.log(game);
+{
+    var mobile = "VIVO"
+    console.log(mobile);
+}
+console.log(mobile);
+
+// let name = "khan"
+// console.log(name);
+
+var mobile = "Oppo"
+console.log(mobile);
+                            // 5 PRIMITIVE DATATYPES
+
+{
+    let name = "Khuzaima khan"
+    console.log(name);
+    let age = 19
+    console.log(age);
+    let city = "dera Ismail khan"
+    console.log(city);
+    let student = false
+    console.log(student);
+    let language = "Urdu"
+    console.log(language);
+    let x ;
+    console.log(x);
+    let n = null
+    console.log(n);
+    // console.log(typeof name)
+    // console.log(typeof age)
+    // console.log(typeof city)
+    // console.log(typeof language) 
+    // console.log(typeof student) 
+    // console.log(typeof n) 
+    // console.log(typeof x) 
+}
+
+                            // Object
+
+let student ={
+    name : "khuzaima", age : 18 , game : 'football' , city : "dera isamil khan" 
+}
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.game);
+// console.log(student.city);
+
+console.log(student["age"]);
+console.log(student["city"]);
+console.log(student["game"]);
+console.log(student["name"]);
+
