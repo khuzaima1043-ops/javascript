bb50e2a65d4e9e0d8af58bc9fa50b1df8314b2ec
khuzaima khan <khuzaima1043@gmail.com>
2026-08-20 15:31:57 +0500

    file name change and new comparison file add

---
+++ Chp#2/class 6/assignments operators in .js
@@ -0,0 +1,20 @@
+let a =5
+let b = 7
+console.log(a=b);
+
+let c =5
+ c+=1
+ console.log(c);
+ 
+
+let d =5
+ d-=1
+ console.log(d);
+ 
+let e =5
+ e/=3
+ console.log(e);
+ 
+ let f =5
+ f**=4
+ console.log(f);
