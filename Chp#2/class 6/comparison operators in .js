38fcc9a0a482a84ed40036d58f877eba1492d7ca
khuzaima khan <khuzaima1043@gmail.com>
2026-08-20 15:48:54 +0500

    comparison file add with practice done

---
+++ Chp#2/class 6/comparison operators in .js
@@ -0,0 +1,36 @@
+let a =5
+let b=5
+console.log(a==b);
+
+let c=5 
+let d=6
+console.log(c==d);
+
+let e=5 
+let f=6
+console.log(e!=f);
+
+let g = 4
+let h = 4
+console.log(g===h);
+
+let i = 7
+let  j = "8"
+console.log(i!==j);
+
+let k = 7
+let l = 9 
+console.log(k<l);
+
+let m = 8 
+let n = 4
+console.log(m>n);
+
+let khuzii= 1
+let khan = 2
+console.log(khan>=khuzii);
+
+let ali = 3
+let hmza = 4 
+console.log(ali<=hmza);
+
