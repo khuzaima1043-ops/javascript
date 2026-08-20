cfa699a274cf938230d2c035bc2f257052c25e34
khuzaima khan <khuzaima1043@gmail.com>
2026-08-20 14:20:10 +0500

    operators in js   in class 6 done

---
+++ CHP # 2/class 6.js
@@ -0,0 +1,62 @@
+                    // OPERATORS IN JS 
+
+let a = 10 
+let b = 4
+// console.log("a+b = ", a+b)
+// console.log("a-b = ", a-b)
+// console.log("a/b = ", a/b)
+// console.log("a**b = ", a**b)
+// console.log("a%b = ", a%b)
+
+
+let p = 15
+console.log(--p)         sub 1-15 tota p=14
+console.log(p++)         14 +1 p=15 
+console.log(p)            p=14
+
+
+let q = 3
+console.log(q++)   3+1=4 see 3
+console.log(q++)     4+1 =5 see 4
+console.log(q++)     5+1 =6 see 5
+console.log(q)       6
+
+
+
+let a = 5
+console.log(a--)      5-1 =4 see 5
+console.log(a)         4
+
+
+let b = 5
+console.log(--b)     1-5 =4  see 4
+console.log(b)          4
+
+
+let c = 5
+console.log(c++)    5 +1 = 6  see 5 
+console.log(c++)     6 +1 = 7 see 6
+console.log(c)      7
+
+
+let r = 7
+console.log(++r + r++)     1+7=8 see 8    +    8+1 = 9    see 8 .    seee8 + see 8 = 16 
+console.log(r)                      toal =9
+
+
+let a = 4
+console.log(a++)     4+1 =5  see 4
+console.log(a++)      5+1=6 see 5
+console.log(--a)       1-6  =6 see 5
+
+
+let b = 10
+console.log(b-- + --b)    10-1=9 see 10     +    1-9=8 see 8        seee 10 + 8 = 18 
+
+
+let c = 5
+c = c + 1      c=5+1=6   c=6   
+console.log(c++)        6+1=7 see 6
+console.log(c)          see 7
+
+
