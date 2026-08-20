b8e00766e6b851d81d96805412f40f3c83f52fc2
khuzaima khan <khuzaima1043@gmail.com>
2026-08-20 16:14:52 +0500

    logical operator file done with practice

---
+++ Chp#2/class 6/logical operator in .js
@@ -0,0 +1,14 @@
+let a = 5 
+let b = 6
+console.log(a<b && a!=b);   // both true =true     // one value  false  =  false  
+
+let c = 5
+let d = 9
+console.log(c<d || c==d);      // one value true 
+
+
+ let e = 5 
+ let f = 9 
+ console.log(!true);       //  true --- false       // false --- true 
+ 
+
