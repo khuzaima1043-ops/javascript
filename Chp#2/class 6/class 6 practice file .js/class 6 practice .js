a76ddf29ba18f41f7b46c440f86cc49c08e21240
khuzaima khan <khuzaima1043@gmail.com>
2026-08-21 15:02:37 +0500

    comlete revision class 1 to 6 done

---
+++ Chp#2/class 6/class 6 practice file .js/class 6 practice .js
@@ -0,0 +1,154 @@
+let name = "khuzii"
+console.log(name);
+let age = 18 
+age= 19
+console.log(age);
+const country = "USA"
+console.log(country);
+// country = "Pakistan"
+// console.log(country);
+
+{
+    var aliage = 18
+    console.log(aliage);
+}
+  console.log(aliage);
+
+var aliage = 19
+console.log(aliage);
+
+{ 
+    let blockage= 20 
+    console.log(blockage);
+}
+// console.log(blockage);
+
+
+                            // SECTION B
+let a ="khuzaima"
+let khuzaimaage = 18
+let u 
+let n = null
+let student = false 
+
+console.log(typeof a);
+console.log(typeof khuzaimaage);
+console.log(typeof u);
+console.log(typeof n);
+console.log(typeof student);
+
+let person = {
+    name: "khuzaima" , city : "d-i-khan" ,  language : " Urdu" , color : "brown"
+}
+console.log(person.name);
+console.log(person.city);
+console.log(person["language"]);
+console.log(person["color"]);
+
+
+                                    // SECTION C 
+
+let ali = "khan"
+let khan = 18
+console.log(ali+ (khan) );
+console.log(typeof (ali+khan));
+
+
+
+const object= {
+    year : 19 ,  
+    flower : false , 
+    cpu : "voilet" ,
+    
+}
+ console.log(object);
+ object.age=17
+ console.log(object);
+
+                                              // SECTION D
+
+let f = 10
+console.log(f++);     
+console.log(++f);     
+console.log(f--);       
+console.log(--f);      
+
+let s=17
+console.log(s++);       
+console.log(++s);    
+console.log(s--);      
+console.log(--s);     
+console.log(s);     
+
+let x =4 
+console.log(++x + x++);  
+console.log(x);   
+
+
+                                      // SECTION  E
+
+let l=5
+l+=1
+console.log(l);
+
+let k=5
+k-=1
+console.log(k);
+
+let i=5
+i/=4
+console.log(i);
+
+let m=5
+m*=3
+console.log(m);
+
+                                  // SECTION  F
+
+let jim=2
+let kenny=4
+console.log(jim==kenny);
+
+
+let  jan=2
+let boyd=4
+console.log(jan!=boyd);
+
+let khokhar=4
+let yousaf=4
+console.log(khokhar==yousaf);
+
+
+let red=5
+let pink=7
+console.log(pink>red);
+
+let yellow= 9
+let green = 10
+console.log(yellow<=green);
+
+let blue=67
+let grey=78
+console.log(blue>=grey);
+
+
+                                  // SECTION  G
+
+let white=4
+let black=3
+console.log(black==white && white!=black );   
+
+
+
+let golden=9
+let peach=34
+console.log(golden<peach ||golden==peach);      // bacause one vaue is correct 
+
+
+let v=45
+let z = 34
+console.log(!false);
+
+let q=44
+let p = 39
+console.log(!true);
