ced2a52c0f0506f2e19a2d12a0ca9a12b8ff600f
khuzaima khan <khuzaima1043@gmail.com>
2026-09-06 15:40:37 +0500

    Add CHP # 5  and thir files . Add introduction to arrays and value manipulation examples in class # 16 practice set

---
+++ Chp # 5/class # 16/class 16 practice set.js
@@ -0,0 +1,19 @@
+let a = [112,25 ,33,  45,47 ,85,55, "kjhuzaima" , false , true , null ]
+ a[0] = 135 //changing value with the heplp of index 
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
+console.log(a[10]);
+console.log(a[11]);  // because totsl values or 10 thn 10 values  will be printed 
+ console.log(a.length);
+ console.log(typeof a);
+ 
---
+++ Chp # 5/class # 16/introduction to arrays .js
@@ -0,0 +1,36 @@
+// Arrays are variables which can hold more than one value like objects , few number, few booleans , few names etc 
+
+// ACCESSING VALUES 
+let a = [1 , 2 ,3 , 4 , 5 ,false , "not present" , null  ]
+// value ko access krny k eliyw hamesha index use hota ha or index hamesha 0 s shuru hota ha 
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+
+
+// find the length 
+let l= ["khuzaima khan" , 1 , 2 ,3  ,4 ,5  ,6  ,7  , false , true]
+// lenth hmy btata ha ky array ma total values ki quantity kia ha 
+console.log(l.length);
+
+// Changing the value 
+let changevalue = [1,2,3,4,5,6,4, "Khuzaima" ,"khan"]
+changevalue[0] = 23
+console.log(changevalue[0]);
+console.log(changevalue[1]);
+console.log(changevalue[2]);
+console.log(changevalue[3]);
+console.log(changevalue[4]);
+console.log(changevalue[5]);
+console.log(changevalue[6]);
+console.log(changevalue[7]);
+
+// numbers are meutable  
+// Araays are meutable 
+// Arrays can be changed 
+
+
+// IN javascript  Arrays is ot a permitive type . Array is always an object  
+
+console.log(typeof changevalue);
