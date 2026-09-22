7e99c1e1256023fd1d4bd38447f67d0c4f7e1ef3
khuzaima khan <khuzaima1043@gmail.com>
2026-09-22 17:59:24 +0500

    Add console methods examples to demonstrate logging, timing, and assertions

---
+++ Chp # 6/console methods/index.js
@@ -0,0 +1,21 @@
+        // CONSOLE METHODS 
+
+        console.log("This is a log message"); 
+
+        console.info("This is an info message");
+
+        console.warn("This is a warning message");
+
+        console.error("This is an error message");
+
+        console.time("starting time of funtion or anything");
+
+        console.timeEnd("ending time of function ");
+
+        let object = {a:1, b:2, c:3};
+        console.table(object);
+
+        console.assert("this method takes a condition and if the condition is false then it will throw an error" )
+
+        console.clear("this `method clears the console");
+
